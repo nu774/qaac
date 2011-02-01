09a8ecd8ea7c78a97bdbc0ed33008a221cebe459
nu774 <honeycomb77@gmail.com>
2011-02-02 02:21:39 +0900

    modified RawSource to use PartialSource<>

--- rawsource.h
+++ rawsource.h
@@ -3,42 +3,34 @@
 
 #include "iointer.h"
 
-class RawSource: public ISource {
+class RawSource: public ISource, public PartialSource<RawSource> {
     InputStream m_stream;
     SampleFormat m_format;
-    uint64_t m_duration;
-    uint64_t m_samples_read;
 public:
     explicit RawSource(InputStream &stream, const SampleFormat &format)
-	: m_stream(stream), m_format(format), m_samples_read(0)
+	: m_stream(stream), m_format(format)
     {
-	m_duration = m_stream.size();
-	if (m_duration != -1) m_duration /= format.bytesPerFrame();
+	int64_t len = m_stream.size();
+	setRange(0, len == -1 ? -1 : len / format.bytesPerFrame());
     }
-    void setRange(int64_t start=0, int64_t length=-1)
-    {
-	if (start > 0) {
-	    int64_t bytes = start * m_format.bytesPerFrame();
-	    if (m_stream.seek_forward(bytes) != bytes)
-		throw std::runtime_error("RawSource: seek failed");
-	}
-	int64_t dur = m_duration;
-	if (length >= 0 && (dur == -1 || length < dur))
-	    m_duration = length;
-    }
-    uint64_t length() const { return m_duration; }
+    uint64_t length() const { return getDuration(); }
     const SampleFormat &getSampleFormat() const { return m_format; }
     const std::vector<uint32_t> *getChannelMap() const { return 0; }
     size_t readSamples(void *buffer, size_t nsamples)
     {
-	uint64_t rest = m_duration - m_samples_read;
-	nsamples = static_cast<size_t>(
-		std::min(static_cast<uint64_t>(nsamples), rest));
-	if (!nsamples) return 0;
-	size_t nblocks = m_format.bytesPerFrame();
-	size_t rc = m_stream.read(buffer, nsamples * nblocks) / nblocks;
-	m_samples_read += rc;
-	return rc;
+	nsamples = adjustSamplesToRead(nsamples);
+	if (nsamples) {
+	    size_t nblocks = m_format.bytesPerFrame();
+	    nsamples = m_stream.read(buffer, nsamples * nblocks) / nblocks;
+	    addSamplesRead(nsamples);
+	}
+	return nsamples;
+    }
+    void skipSamples(int64_t count)
+    {
+	int64_t bytes = count * m_format.bytesPerFrame();
+	if (m_stream.seek_forward(bytes) != bytes)
+	    throw std::runtime_error("RawSource: seek failed");
     }
 };
 
