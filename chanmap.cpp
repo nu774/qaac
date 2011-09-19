2bb3e5b04ffa8d9e7d87b169669619b85cda07e0
nu774 <honeycomb77@gmail.com>
2011-09-19 13:54:29 +0900

    disabled L R C Cs 4ch input

--- chanmap.cpp
+++ chanmap.cpp
@@ -21,12 +21,11 @@ GetChannelLayoutTagFromChannelMap(const std::vector<uint32_t>& chanmap)
     case 0x3: return kAudioChannelLayoutTag_Stereo;
     case 0x7: return kAudioChannelLayoutTag_MPEG_3_0_A;
     case 0x33: return kAudioChannelLayoutTag_Quadraphonic;
-    case 0x107: return kAudioChannelLayoutTag_MPEG_4_0_A;
     case 0x37: case 0x607:
 	return kAudioChannelLayoutTag_MPEG_5_0_A;
     case 0x3f: case 0x60f:
 	return kAudioChannelLayoutTag_MPEG_5_1_A;
-    case 0x13f: case 0x70f:
+    case 0x13f:
 	return kAudioChannelLayoutTag_MPEG_6_1_A;
     case 0xff: case 0x63f: return kAudioChannelLayoutTag_MPEG_7_1_C;
     }
@@ -37,19 +36,15 @@ uint32_t GetAACChannelMapFromLayoutTag(uint32_t tag,
 	std::vector<uint32_t> *result)
 {
     static const uint32_t a30[] = { 3, 1, 2, 0 };
-    static const uint32_t a40[] = { 3, 1, 2, 4, 0 };
     static const uint32_t a50[] = { 3, 1, 2, 4, 5, 0 };
     static const uint32_t a51[] = { 3, 1, 2, 5, 6, 4, 0 };
     static const uint32_t a61[] = { 3, 1, 2, 5, 6, 7, 4, 0 };
-    //static const uint32_t a71[] = { 3, 7, 8, 1, 2, 5, 6, 4, 0 };
     static const uint32_t a71[] = { 3, 1, 2, 7, 8, 5, 6, 4, 0 };
     const uint32_t *a = 0;
     uint32_t newtag = 0;
     switch (tag) {
     case kAudioChannelLayoutTag_MPEG_3_0_A:
 	a = a30; newtag = kAudioChannelLayoutTag_AAC_3_0; break;
-    case kAudioChannelLayoutTag_MPEG_4_0_A:
-	a = a40; newtag = kAudioChannelLayoutTag_AAC_4_0; break;
     case kAudioChannelLayoutTag_MPEG_5_0_A:
 	a = a50; newtag = kAudioChannelLayoutTag_AAC_5_0; break;
     case kAudioChannelLayoutTag_MPEG_5_1_A:
