47688ae88a4ead3a10266c37c352f130e2d5195d
nu774 <honeycomb77@gmail.com>
2013-10-07 01:12:19 +0900

    saner interface of util::pack(), util::unpack()

--- util.cpp
+++ util.cpp
@@ -50,27 +50,27 @@ namespace util {
     }
 
     template <typename X, typename Y>
-    void packXtoY(void *data, size_t *size)
+    void packXtoY(void *data, size_t count)
     {
         const X *src = static_cast<X*>(data);
         Y *dst = static_cast<Y*>(data);
-        const int count = static_cast<int>(*size / sizeof(X));
         const int shifts = (sizeof(X) - sizeof(Y)) * 8;
         
-        for (int i = 0; i < count; ++i)
+        for (size_t i = 0; i < count; ++i)
             dst[i] = static_cast<Y>(src[i] >> shifts);
-        *size = count * sizeof(Y);
     }
 
     void pack(void *data, size_t *size, unsigned width, unsigned new_width)
     {
         if (width == new_width)
             return;
-        else if (width == 4 && new_width == 1)
-            packXtoY<uint32_t, uint8_t>(data, size);
-        else if (width == 4 && new_width == 2)
-            packXtoY<uint32_t, uint16_t>(data, size);
-        else if (width == 4 && new_width == 3) {
+        else if (width == 4 && new_width == 1) {
+            packXtoY<uint32_t, uint8_t>(data, *size / 4);
+            *size /= 4;
+        } else if (width == 4 && new_width == 2) {
+            packXtoY<uint32_t, uint16_t>(data, *size / 4);
+            *size /= 2;
+        } else if (width == 4 && new_width == 3) {
             const uint8_t *src = static_cast<uint8_t*>(data);
             uint8_t *dst = static_cast<uint8_t*>(data);
             const size_t count = *size / 4;
@@ -88,16 +88,11 @@ namespace util {
     }
 
     template <typename X, typename Y>
-    void unpackXtoY(const void *input, void *output, size_t *size)
+    void unpackXtoY(const X *src, Y *dst, size_t count)
     {
-        const X *src = static_cast<const X*>(input);
-        Y *dst = static_cast<Y*>(output);
-        const int count = static_cast<int>(*size / sizeof(X));
         const int shifts = (sizeof(Y) - sizeof(X)) * 8;
-        
-        for (int i = 0; i < count; ++i)
+        for (size_t i = 0; i < count; ++i)
             dst[i] = static_cast<Y>(src[i] << shifts);
-        *size = count * sizeof(Y);
     }
 
     void unpack(const void *input, void *output, size_t *size, unsigned width,
@@ -105,11 +100,15 @@ namespace util {
     {
         if (width == new_width)
             std::memcpy(output, input, *size);
-        else if (width == 1 && new_width == 4)
-            unpackXtoY<uint8_t, uint32_t>(input, output, size);
-        else if (width == 2 && new_width == 4)
-            unpackXtoY<uint16_t, uint32_t>(input, output, size);
-        else if (width == 3 && new_width == 4) {
+        else if (width == 1 && new_width == 4) {
+            unpackXtoY(static_cast<const uint8_t *>(input),
+                       static_cast<uint32_t *>(output), *size);
+            *size *= 4;
+        } else if (width == 2 && new_width == 4) {
+            unpackXtoY(static_cast<const uint16_t *>(input),
+                       static_cast<uint32_t *>(output), *size / 2);
+            *size *= 2;
+        } else if (width == 3 && new_width == 4) {
             const uint8_t *src = static_cast<const uint8_t*>(input);
             uint8_t *dst = static_cast<uint8_t*>(output);
             const size_t count = *size / 3;
