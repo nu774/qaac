7ece5372c3c7a0b157c3de08f8a3ef090880510c
nu774 <honeycomb77@gmail.com>
2017-04-03 19:40:03 +0900

    add spece after string literal for c++11 conformance

--- mp4v2/src/mp4util.h
+++ mp4v2/src/mp4util.h
@@ -33,7 +33,7 @@ namespace mp4v2 { namespace impl {
 #ifndef ASSERT
 #   define ASSERT(expr) \
         if (!(expr)) { \
-            throw new Exception("assert failure: "LIBMPV42_STRINGIFY((expr)), __FILE__, __LINE__, __FUNCTION__ ); \
+            throw new Exception("assert failure: " LIBMPV42_STRINGIFY((expr)), __FILE__, __LINE__, __FUNCTION__ ); \
         }
 #endif
 
