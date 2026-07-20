1aaea1887888e5995a0c09a3090c3a241e1a651d
nu774 <honeycomb77@gmail.com>
2026-07-20 20:21:13 +0900

    mp4v2: add config headers for mingw

---
+++ mp4v2/libplatform/config.h
@@ -0,0 +1,50 @@
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdio.h> header file. */
+#define HAVE_STDIO_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if LFS should be activated */
+/* #undef NEED_LFS_ACTIVATION */
+
+/* Name of package */
+#define PACKAGE "mp4v2"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "support@mp4v2.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "MP4v2"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "MP4v2 2.1.3"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "mp4v2"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "https://mp4v2.org"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2.1.3"
+
+/* Version number of package */
+#define VERSION "2.1.3"
